2f0e83219ac43836ae00b7c2fd6cb573aa5c8961
Filipe Lukebana <31851972+LUK3D@users.noreply.github.com>
2024-02-17 02:39:07 -0300

    🔥 Add Command editor UI

--- mimmo_web_interface/src/App.tsx
+++ mimmo_web_interface/src/App.tsx
@@ -1,9 +1,10 @@
+import CommandEditor from "./features/comand_editor"
 
 function App() {
   return (
     <>
-      <div className="w-full h-full">
-        
+      <div className="w-full h-full flex flex-col">
+        <CommandEditor></CommandEditor>
       </div>
     </>
   )
