9b583e6fe69d98de9806fe5f2a72b02841c42854
Filipe Lukebana <31851972+LUK3D@users.noreply.github.com>
2024-02-17 02:37:15 -0300

    Update styles

--- mimmo_web_interface/tailwind.config.js
+++ mimmo_web_interface/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#9747FF",
+      }
+    },
   },
   plugins: [],
 }
